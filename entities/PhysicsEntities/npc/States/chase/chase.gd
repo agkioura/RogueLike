f19f882a389e77abfa4c3c8a3e1588cce82ab715
agkioura <agkioura@csd.auth.gr>
2025-04-26 22:44:41 +0300

    fixed facing direction for enemy while chasing

--- entities/PhysicsEntities/npc/States/chase/chase.gd
+++ entities/PhysicsEntities/npc/States/chase/chase.gd
@@ -4,19 +4,33 @@ class_name Chase extends State
 
 @onready var nav: NavigationAgent2D = $"../../NavigationAgent2D"
 
+var approachRadius: int = 30
+
 func enterState() -> void:
 	super()
 	print("Enemy entered chase state")
 	if parent.target:
+		parent.animation.play(animationName)
 		nav.target_position = parent.target.global_position
-
 	
 func processPhysics(delta : float) -> State:
 	if !parent.target:
 		return patrol
 		
 	var dir = to_local(nav.get_next_path_position()).normalized()
-	parent.velocity = dir * parent.speed
+	var distanceFromTarget := sqrt(pow(parent.global_position.x - parent.target.global_position.x, 2) + pow(parent.global_position.y - parent.target.global_position.y, 2))
+	if distanceFromTarget > approachRadius:
+		parent.velocity = dir * parent.speed
+	else:
+		parent.velocity = Vector2.ZERO
+		
+	var directionToTarget := Vector2(parent.target.global_position.x - parent.global_position.x, parent.target.global_position.y - parent.global_position.y)
+	if directionToTarget.x > 0:
+		if $"../../Sprite2D".scale.x == -1:
+			$"../../Sprite2D".scale.x *= -1
+	else:
+		if $"../../Sprite2D".scale.x == 1:
+			$"../../Sprite2D".scale.x *= -1
 	parent.move_and_slide()
 	return null
 
